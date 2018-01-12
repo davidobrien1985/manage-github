f8a0dc20f5d8fb209c3f181e7049fe0c2ecd9e4d
David OBrien <me@david-obrien.net>
2018-01-12 17:36:16 +1100

    reintroduce patchasync

--- githubmanage/helper/helper.cs
+++ githubmanage/helper/helper.cs
@@ -10,6 +10,29 @@ using Newtonsoft.Json.Linq;
 namespace githubmanage.helper
 
 {
+  public static class HttpClientExtensions
+  {
+    public static async Task<HttpResponseMessage> PatchAsync(this HttpClient client, Uri requestUri, HttpContent iContent)
+    {
+      var method = new HttpMethod("PATCH");
+      var request = new HttpRequestMessage(method, requestUri)
+      {
+        Content = iContent
+      };
+
+      HttpResponseMessage response = new HttpResponseMessage();
+      try
+      {
+        response = await client.SendAsync(request);
+      }
+      catch (TaskCanceledException e)
+      {
+        Debug.WriteLine("ERROR: " + e.ToString());
+      }
+
+      return response;
+    }
+  }
   public class Helper
   {
     public static JObject CreateMasterBranch(string apiUri, string githubOrg, string repoName, string githubPat, ILambdaContext context)
